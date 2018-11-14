97d75d1a46c8dc119ff74a1072110529c445a8d9
lesach <GIT_malmaison01>
2018-11-14 22:33:42 +0100

    Building GUI

--- trading-tool-gui/src/main/java/TradingToolForm.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public class TradingToolForm {
-    public JPanel mainPanel;
-    private JButton btnProductSearch;
-    private JTextField txtProductSearch;
-    private JLabel lblAmount;
-    private JTextField txtAvailableCashValue;
-}
